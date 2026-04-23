ed3280bf21c1a464cdf6573239b09a7a8f8e00e2
targed <ltklionel@gmail.com>
2026-04-23 12:32:03 -0500

    Added gemini results

--- evaluate.py
+++ evaluate.py
@@ -82,13 +82,13 @@ if __name__ == '__main__':
     parser.add_argument('dataset_file', help='Dataset file')
     parser.add_argument('prediction_file', help='Prediction File')
     args = parser.parse_args()
-    with open(args.dataset_file) as dataset_file:
+    with open(args.dataset_file, encoding='utf-8') as dataset_file:
         dataset_json = json.load(dataset_file)
         if (dataset_json['version'] != expected_version):
             print('Evaluation expects v-' + expected_version +
                   ', but got dataset with v-' + dataset_json['version'],
                   file=sys.stderr)
         dataset = dataset_json['data']
-    with open(args.prediction_file) as prediction_file:
+    with open(args.prediction_file, encoding='utf-8') as prediction_file:
         predictions = json.load(prediction_file)
     print(json.dumps(evaluate(dataset, predictions)))
